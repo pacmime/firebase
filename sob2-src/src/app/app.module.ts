7f63f336b9d8ba7d2a16183758820ab636a1b4fe
Patrick Neal <pacmime@gmail.com>
2020-01-22 11:07:20 -0500

    replaced v1 app with v2-material

--- sob2-src/src/app/app.module.ts
+++ sob2-src/src/app/app.module.ts
@@ -150,7 +150,8 @@ const appRoutes: Routes = [
     { path: 'chars',        component: CharListComponent,   canActivate: [AuthGuard] },
     { path: 'chars/:id',    component: CharacterComponent,  canActivate: [AuthGuard] },
     { path: 'import',       component: UploadComponent },
-    { path: '',             redirectTo: '/chars', pathMatch: 'full' }
+    { path: '',             redirectTo: '/chars', pathMatch: 'full' },
+    { path: '**',           redirectTo: '/chars' }
 
 ];
 
